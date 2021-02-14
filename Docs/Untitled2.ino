0ab54c336e16d60b963f659692b66307e39059d6
Oran C <Oranbusiness@gmail.com>
2021-02-13 21:11:18 -0800

    [add] hardware support changes

--- Docs/Untitled2.ino
+++ Docs/Untitled2.ino
@@ -1,10 +1,12 @@
 #include <Arduino.h>
 
+
 // Configuration
 // Minimum tested unit length was 10ms and it works reliably with cheap light resistor.
 //[Adaptive LED Morse Code Decoder and Timer Interrupt - Arduino Project Hub](https://create.arduino.cc/projecthub/shjin/adaptive-led-morse-code-decoder-and-timer-interrupt-8d18a7)
 const int UNIT_LENGTH = 100;
 const int BUFFER_SIZE = 5;
+
 
 
 enum class Signal : byte {
@@ -16,10 +18,12 @@ enum class Signal : byte {
   LONGGAP = 5
 };
 
+
 struct MorseCodeElement {
   Signal m_signal;
   unsigned long m_duration;
 };
+
 
 class MorseCodeBuffer {
     int m_size;
@@ -27,10 +31,12 @@ class MorseCodeBuffer {
     int m_tail;
     MorseCodeElement* m_buffer;
 
+
   public:
     MorseCodeBuffer(int size) {
       // Use extra element to distinguish empty vs full.
       size++;
+
 
       m_size = size;
       m_head = 0;
@@ -38,19 +44,24 @@ class MorseCodeBuffer {
       m_buffer = new MorseCodeElement[size];
     }
 
+
     bool Enqueue(MorseCodeElement element) {
       int new_tail = (m_tail + 1) % m_size;
+
 
       // Is full?
       if (new_tail == m_head) {
         return false;
       }
 
+
       m_tail = new_tail;
       m_buffer[m_tail] = element;
 
+
       return true;
     }
+
 
     bool TryDequeue(MorseCodeElement* element) {
       // Is empty?
@@ -58,19 +69,23 @@ class MorseCodeBuffer {
         return false;
       }
 
+
       *element = m_buffer[m_head];
       m_head = (m_head + 1) % m_size;
       return true;
     }
+
 
     int GetCount() {
       if (m_head == m_tail) {
         return 0;
       }
 
+
       return (m_tail - m_head + m_size) % m_size;
     }
 };
+
 
 class AdaptiveLogicLevelProcessor {
     int m_sensorMinValue = 1023;
@@ -78,6 +93,7 @@ class AdaptiveLogicLevelProcessor {
     int m_sensorMedianValue = 511;
     unsigned long m_sensorCalibrationTime = 0;
     bool m_calibrated;
+
 
   public:
     AdaptiveLogicLevelProcessor() {
@@ -87,12 +103,15 @@ class AdaptiveLogicLevelProcessor {
       m_sensorCalibrationTime = 0;
     }
 
+
     bool process(int sensorValue, int* digitalInputValue) {
       unsigned long currentTime = millis();
+
 
       // Re-calibrate sensor value range
       if (currentTime - m_sensorCalibrationTime > 5000) {
         if (m_sensorMinValue < m_sensorMaxValue) {
+
 
           if (m_sensorMaxValue - m_sensorMinValue > 20) {
             m_sensorMedianValue = m_sensorMinValue + (m_sensorMaxValue - m_sensorMinValue) / 2;
@@ -108,31 +127,38 @@ class AdaptiveLogicLevelProcessor {
           }
         }
 
+
         m_sensorMaxValue = 0;
         m_sensorMinValue = 1023;
         m_sensorCalibrationTime = currentTime;
       }
 
+
       if (m_sensorMinValue > sensorValue) {
         m_sensorMinValue = sensorValue;
       }
+
 
       if (m_sensorMaxValue < sensorValue) {
         m_sensorMaxValue = sensorValue;
       }
 
+
       if (!m_calibrated) {
         return false;
       }
+
 
       *digitalInputValue = sensorValue > m_sensorMedianValue ? HIGH : LOW;
       return true;
     }
 };
 
+
 class MorseCodeElementProcessor {
     unsigned long m_previousTime = 0;
     int m_previousSignal = LOW;
+
 
     int m_oneUnitMinValue;
     int m_oneUnitMaxValue;
@@ -141,29 +167,36 @@ class MorseCodeElementProcessor {
     int m_sevenUnitMinValue;
     int m_sevenUnitMaxValue;
 
+
   public:
     MorseCodeElementProcessor(int unitLengthInMilliseconds) {
       m_oneUnitMinValue = (int)(unitLengthInMilliseconds * 0.5);
       m_oneUnitMaxValue = (int)(unitLengthInMilliseconds * 1.5);
 
+
       m_threeUnitMinValue = (int)(unitLengthInMilliseconds * 2.0);
       m_threeUnitMaxValue = (int)(unitLengthInMilliseconds * 4.0);
+
 
       m_sevenUnitMinValue = (int)(unitLengthInMilliseconds * 5.0);
       m_sevenUnitMaxValue = (int)(unitLengthInMilliseconds * 8.0);
     }
+
 
     bool process(int newSignal, MorseCodeElement* element) {
       unsigned long currentTime = millis();
       unsigned long elapsed;
       bool shouldBuffer = false;
 
+
       element->m_signal = Signal::NOISE;
+
 
       // If previous status was OFF and now it is ON
       if (m_previousSignal == LOW && newSignal == HIGH) {
         elapsed = currentTime - m_previousTime;
         element->m_duration = elapsed;
+
 
         if (m_sevenUnitMinValue <= elapsed) {
           element->m_signal = Signal::LONGGAP;
@@ -179,11 +212,13 @@ class MorseCodeElementProcessor {
           shouldBuffer = true;
         }
 
+
         m_previousSignal = HIGH;
         m_previousTime = currentTime;
       } else if (m_previousSignal == HIGH && newSignal == LOW) {
         elapsed = currentTime - m_previousTime;
         element->m_duration = elapsed;
+
 
         if (m_threeUnitMinValue <= elapsed && elapsed <= m_threeUnitMaxValue) {
           element->m_signal = Signal::DAH;
@@ -196,13 +231,16 @@ class MorseCodeElementProcessor {
           shouldBuffer = true;
         }
 
+
         m_previousSignal = LOW;
         m_previousTime = currentTime;
       }
 
+
       return shouldBuffer;
     }
 };
+
 
 class MorseCodeProcessor {
   private:
@@ -226,11 +264,13 @@ class MorseCodeProcessor {
       '\0', '\0', '\0', '9', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '0', '\0', '\0', '\0'
     };
 
+
     bool m_error;
     int m_start;
     int m_end;
     int m_index;
     Signal m_previousInput;
+
 
     void reset() {
       m_error = false;
@@ -239,14 +279,17 @@ class MorseCodeProcessor {
       m_index = (m_end - m_start) / 2;
     }
 
+
   public:
     MorseCodeProcessor() {
       reset();
       m_previousInput = Signal::NOISE;
     }
 
+
     bool process(Signal input, char* output) {
       bool completed = false;
+
 
       if (!m_error && input == Signal::DIT) {
         if (m_start == m_index) {
@@ -265,9 +308,11 @@ class MorseCodeProcessor {
       } else if (input == Signal::GAP || input == Signal::LONGGAP) {
         completed = !m_error && tree[m_index] != 0;
 
+
         if (completed) {
           output[0] = tree[m_index];
           output[1] = '\0';
+
 
           if (input == Signal::LONGGAP) {
             output[1] = ' ';
@@ -275,21 +320,27 @@ class MorseCodeProcessor {
           }
         }
 
+
         reset();
       }
 
+
       m_previousInput = input;
+
 
       return completed;
     }
 };
 
+
 constexpr char MorseCodeProcessor::tree[];
+
 
 MorseCodeBuffer buffer(BUFFER_SIZE);
 MorseCodeProcessor morseCodeProcessor;
 AdaptiveLogicLevelProcessor logicLevelProcessor;
 MorseCodeElementProcessor morseCodeElementProcessor(UNIT_LENGTH);
+
 
 
 /************************************************************
@@ -298,34 +349,37 @@ MorseCodeElementProcessor morseCodeElementProcessor(UNIT_LENGTH);
 SIGNAL(TIMER0_COMPA_vect) {
   cli();
 
+
   int digitalInputValue;
+
 
   if (logicLevelProcessor.process(analogRead(A0), &digitalInputValue)) {
     MorseCodeElement element;
+
 
     if (morseCodeElementProcessor.process(digitalInputValue, &element)) {
       buffer.Enqueue(element);
     }
   }
 
+
   sei();
 }
 int lcd_cursor_pos = 0;
 #include <LiquidCrystal.h>
 
+
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
-const int rs = 12, en = 11, 
-d4 = 2,
-d5 = 3,
-d6 = 4,
-d7 = 5;
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
 
 #define NOTE_C4  262
 const int TONE_PIN = 8;
 const int TONE_SHORT_DURATION = 20;
 const int TONE_LONG_DURATION = 80;
+
 
 void da() {
   tone(TONE_PIN, NOTE_C4, 0);
@@ -333,6 +387,7 @@ void da() {
   noTone(TONE_PIN);
   delay(10);
 }
+
 
 void dit() {
   tone(TONE_PIN, NOTE_C4, 0);
@@ -343,17 +398,11 @@ void dit() {
 int i = 0;
 void setup() {
   Serial.begin(9600);
-  Serial.println(F("MorsCode[ ]Readr"));
+  Serial.println(F("Morse Code Decoder"));
   Serial.println("-----------------");
+  lcd.begin(16, 1);
   // Print a message to the LCD.
-  lcd.begin(16, 2);//
-
-//set curser to write on row 1
-  lcd.setCursor(0, 1);  lcd.print("-----------------");
-  lcd.setCursor(0, 0);  lcd.print("MorseCode Decoder");
-  // Print a message to the LCD.
-
-  lcd.setCursor(0, 1);//set cursor to row 2
+  lcd.print("_________________");
   da();
   dit();
   da();
@@ -373,6 +422,7 @@ void setup() {
   sei();
 }
 
+
 /************************************************************
    Helper function to dequeue an item from the buffer safely
  ************************************************************/
@@ -383,49 +433,49 @@ bool TryDequeueSafe(MorseCodeElement* element) {
   bool result = buffer.TryDequeue(element);
   sei();
 
+
   return result;
 }
+
 
 char* output = new char[3];
 // include the library code:
 
 
+
 void loop() {
   MorseCodeElement element;
+
 
   // Drain buffer
   while (TryDequeueSafe(&element)) {
     if (element.m_signal == Signal::DIT) {
       Serial.print(".");
-      // display a dot on the top row
-      lcd.setCursor(9, 0);  
-      lcd.print(".");
-      lcd.setCursor(0, 1);
       //      dit();
     } else if (element.m_signal == Signal::DAH) {
       Serial.print("-");
-      
-      lcd.setCursor(9, 0);  
-      lcd.print("-");
-      lcd.setCursor(0, 1);
+      //      da();
     }
+
 
     if (morseCodeProcessor.process(element.m_signal, output)) {
       Serial.print('(');
       Serial.print(output);
       lcd.setCursor(i, 1);
       lcd.print(output);
-      
       i += 1;
       if (i > 16) {
         i = 0;
               lcd.setCursor(i, 1);
 
+
           lcd.print("                 ");
+
 
       }
       Serial.print(')');
     }
+
 
     if (element.m_signal == Signal::LONGGAP) {
       Serial.println();
